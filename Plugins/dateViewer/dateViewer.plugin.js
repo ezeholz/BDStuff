a5b3591d491da529ecb873c05f909e14e518aaf8
Jacob <30327407+hammy1@users.noreply.github.com>
2018-04-06 19:35:18 +0200

    Update the file.

--- Plugins/dateViewer/dateViewer.plugin.js
+++ Plugins/dateViewer/dateViewer.plugin.js
@@ -5,7 +5,7 @@ class dateViewer {
     getDescription() { return 'Displays current time, date and day of the week on your right side. The way it\'s displayed depends on your locale conventions.' }
     getVersion() { return '0.2.1'; }
     getAuthor() { return 'hammy'; }
-    getDownloadLink() { return 'https://raw.githubusercontent.com/hammy255/BDStuff/master/Plugins/dateViewer/dateViewer.plugin.js'; }
+    getDownloadLink() { return 'https://raw.githubusercontent.com/hammy1/BDStuff/master/Plugins/dateViewer/dateViewer.plugin.js'; }
 
     constructor() {
         this.initialized = false;
