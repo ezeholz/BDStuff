511a2bf4b85f9c138467626e6f22da49404d8734
Akalai Violintzis <hello@akalai.com.ar>
2025-11-29 19:12:29 +0100

    fix: Update version number to 0.3.1 and improve plugin metadata for DateViewer

--- Plugins/dateViewer/dateViewer.plugin.js
+++ Plugins/dateViewer/dateViewer.plugin.js
@@ -2,7 +2,7 @@
  * @name DateViewer
  * @author ezeholz
  * @authorId 820741927401160714
- * @version 0.3.0
+ * @version 0.3.1
  * @description Deprecated as the updater now wrongly tries to update it.
  * @website https://ezeholz.com.ar/
  * @source https://github.com/ezeholz/BDStuff/tree/main/Plugins/BetterDateViewer
@@ -12,11 +12,24 @@
 const fileSystem = require("fs");
 
 module.exports = (() => {
+	const config = {
+        info: {
+            name: "DateViewer",
+            authors: [
+                { name: "hammy", discord_id: "256531049222242304", github_username: "hammy1" },
+                { name: "ezeholz", discord_id: "820741927401160714", github_username: "ezeholz" }
+            ],
+            version: "0.3.1",
+            description: "Deprecated as the updater now wrongly tries to update it. Use BetterDateViewer instead.",
+            github: "https://github.com/ezeholz/BDStuff/tree/main/Plugins/BetterDateViewer",
+            github_raw: "https://raw.githubusercontent.com/ezeholz/BDStuff/main/Plugins/BetterDateViewer/BetterDateViewer.plugin.js"
+        },
+	};
     return class DateViewer {
         constructor(meta) {for (let key in meta) {
 			if (!this[key]) this[key] = meta[key];
 		}}
-        getName () {return this.name;}
+		getName () {return this.name;}
 		getAuthor () {return this.author;}
 		getVersion () {return this.version;}
 		getDescription () {return this.description;}
